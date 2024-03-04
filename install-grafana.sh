a898732e618d98eae740440c49839931d7b8892a
sadawap <136122624+sadawap@users.noreply.github.com>
2024-03-04 10:23:30 +0530

    Update install-grafana.sh

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,6 +1,6 @@
-sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
-sudo dpkg -i grafana_7.3.4_amd64.deb
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/oss/release/grafana_10.3.3_amd64.deb
+sudo dpkg -i grafana_10.3.3_amd64.deb
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
